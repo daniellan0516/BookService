7f1ef2727b45ec808d1215982ae348aa84c19408
Daneil Lan <daniellan@aitc.com.tw>
2022-08-09 09:48:26 +0100

    新增Spring Admin

--- src/main/java/tw/com/aitc/sa/SpringCloud/SpintgCloudApplication.java
+++ src/main/java/tw/com/aitc/sa/SpringCloud/SpintgCloudApplication.java
@@ -3,10 +3,10 @@ package tw.com.aitc.sa.SpringCloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import springfox.documentation.oas.annotations.EnableOpenApi;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
-//@EnableOpenApi
 @SpringBootApplication
+@EnableAdminServer
 public class SpintgCloudApplication {
 
 	public static void main(String[] args) {
